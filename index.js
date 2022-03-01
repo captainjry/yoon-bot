2a47a085b4fc601e780ecca780338e0e7e9ba6bf
Archawin Tirugsapun <zepalzth@gmail.com>
2022-03-02 01:21:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -53,6 +53,7 @@ client.on('messageCreate', async message => {
       } else if (text.startsWith('.ปิสอนเชิง')) {
         playSong(connection, './assets/voices/poomteaching.mp3')
       } else if (text.startsWith('.กระจอก')) {
+	console.log("กระจอก")
         playSong(connection, './assets/voices/kk_i_here.mp3')
       }
     } catch (error) {
